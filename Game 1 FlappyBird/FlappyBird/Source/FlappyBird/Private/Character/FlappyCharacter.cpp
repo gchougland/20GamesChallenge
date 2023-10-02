6140b0b3b6b80a1fa78e619faf5cdbabdc00ef49
Gibson Hougland <gchougland@gmail.com>
2023-10-02 08:24:48 -0700

    Started on flappy controls

---
+++ Game 1 FlappyBird/FlappyBird/Source/FlappyBird/Private/Character/FlappyCharacter.cpp
@@ -0,0 +1,53 @@
+﻿// 
+
+
+#include "FlappyBird/Public/Character/FlappyCharacter.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Camera/CameraActor.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+AFlappyCharacter::AFlappyCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	GetCapsuleComponent()->SetSimulatePhysics(true);
+	GetCapsuleComponent()->SetEnableGravity(true);
+}
+
+void AFlappyCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	check(FlappyContext);
+
+	APlayerController* PlayerController = Cast<APlayerController>(GetController());
+	check(PlayerController);
+	if(UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+	{
+		Subsystem->AddMappingContext(FlappyContext, 0);
+	}
+
+	PlayerController->SetViewTargetWithBlend(GameCamera);
+}
+
+void AFlappyCharacter::Flap(const FInputActionValue& Value)
+{
+	GetCapsuleComponent()->AddImpulse(FVector(0,0,FlapImpulse));
+}
+
+void AFlappyCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AFlappyCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	EnhancedInputComponent->BindAction(FlapAction, ETriggerEvent::Started, this, &ThisClass::Flap);
+}
+
---
+++ Game 1 FlappyBird/FlappyBird/Source/FlappyBird/Public/Character/FlappyCharacter.h
@@ -0,0 +1,42 @@
+﻿// 
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PaperCharacter.h"
+
+#include "FlappyCharacter.generated.h"
+
+struct FInputActionValue;
+class UInputAction;
+class UInputMappingContext;
+
+UCLASS()
+class FLAPPYBIRD_API AFlappyCharacter : public APaperCharacter
+{
+	GENERATED_BODY()
+
+public:
+	AFlappyCharacter();
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere)
+	float FlapImpulse = 100.f;
+	
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<ACameraActor> GameCamera;
+
+private:
+
+	UPROPERTY(EditDefaultsOnly, Category=Input)
+	TObjectPtr<UInputMappingContext> FlappyContext;
+
+	UPROPERTY(EditDefaultsOnly, Category=Input)
+	TObjectPtr<UInputAction> FlapAction;
+
+	void Flap(const FInputActionValue& Value);
+};
